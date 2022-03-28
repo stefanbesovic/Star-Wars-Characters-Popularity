ca817f5cdf72d671d2b83be694dd995f165d3a1d
stefanbesovic <stefan.besovic@axiomq.com>
2022-03-28 14:36:50 +0200

    Initial commit.

---
+++ src/test/java/com/axiomq/starwars/StarwarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiomq.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
