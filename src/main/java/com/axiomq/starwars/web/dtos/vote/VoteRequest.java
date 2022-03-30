2e9b3c802490fc2e86b15323db87ed0fa66040af
stefanbesovic <stefan.besovic@axiomq.com>
2022-03-30 15:32:31 +0200

    Create Vote Controller
    Created Vote Controller and necessary DTO entities, with changed icon upload implementation.

---
+++ src/main/java/com/axiomq/starwars/web/dtos/vote/VoteRequest.java
@@ -0,0 +1,24 @@
+package com.axiomq.starwars.web.dtos.vote;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VoteRequest {
+
+    @NotNull
+    @Range(min = 1, max = 10, message = "Value must be between 1 and 10.")
+    private Integer value;
+
+    @Size(max = 120, message = "Comment can hold only 120 characters.")
+    private String comment;
+}
