fdd5f1ee2976d8f8bf0abc1a99fb61535dccb221
stefanbesovic <stefan.besovic@axiomq.com>
2022-04-19 13:21:54 +0200

    Add swagger configuration
    Implemented swagger configuration using springdoc dependency.

--- src/main/java/com/axiomq/starwars/exceptions/ErrorDetails.java
+++ src/main/java/com/axiomq/starwars/exceptions/ErrorDetails.java
@@ -1,16 +1,26 @@
 package com.axiomq.starwars.exceptions;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Timestamp;
 import java.util.Map;
 
+@Schema(description = "Error details template for whole application")
 @Data
 @Builder
 public class ErrorDetails {
+
+    @Schema(description = "Timestamp of error")
     Timestamp timestamp;
+
+    @Schema(description = "Error path")
     String path;
+
+    @Schema(description = "Error message")
     String message;
+
+    @Schema(description = "List of errors")
     Map<String, String> validationErorr;
 }
